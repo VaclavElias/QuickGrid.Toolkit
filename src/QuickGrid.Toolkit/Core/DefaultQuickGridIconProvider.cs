630de4ffe539843276b66ae5c8caeebdf10bdbf2
Vaclav Elias <4528464+VaclavElias@users.noreply.github.com>
2026-08-18 23:22:04 +0100

    feat(blazor): DefaultQuickGridIconProvider.cs - add ClearSearch icon mapping
    - Map QuickGridIcon.ClearSearch to Bootstrap "x-circle" with "me-2" spacing

--- src/QuickGrid.Toolkit/Core/DefaultQuickGridIconProvider.cs
+++ src/QuickGrid.Toolkit/Core/DefaultQuickGridIconProvider.cs
@@ -10,6 +10,8 @@ public class DefaultQuickGridIconProvider : IQuickGridIconProvider
             => builder.AddMarkupContent(0, "<i class=\"bi bi-gear\"></i>"),
         QuickGridIcon.Search => builder
             => builder.AddMarkupContent(0, "<i class=\"bi bi-search me-2\"></i>"),
+        QuickGridIcon.ClearSearch => builder
+            => builder.AddMarkupContent(0, "<i class=\"bi bi-x-circle me-2\"></i>"),
         QuickGridIcon.Filter => builder
             => builder.AddMarkupContent(0, "<i class=\"bi bi-funnel\"></i>"),
         QuickGridIcon.Export => builder
